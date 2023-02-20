500aa4066dd0da5f138c0ff05e50cb04cd204faf
root <joosepkorela@gmail.com>
2023-02-20 21:19:57 +0200

    java config added

--- src/main/java/org/sda/Main.java
+++ src/main/java/org/sda/Main.java
@@ -1,7 +1,23 @@
 package org.sda;
 
+import org.sda.beans.MyBean;
+import org.sda.configuration.ApplicationConfiguration;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+/**
+ * @author Joosep Korela
+ * @Date ${DATE}
+ */
 public class Main {
     public static void main(String[] args) {
+        AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext();
+        applicationContext.register(ApplicationConfiguration.class);
+        applicationContext.refresh();
 
+        MyBean myBean = applicationContext.getBean(MyBean.class);
+        System.out.println(myBean.sayHello());
+
+        myBean.setName("John");
+        System.out.println(myBean.sayHello());
     }
 }
